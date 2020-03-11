56b98a0c019d34d9ab611ce4dc363ae9512bbde9
Sam <lzanzottera@gmail.com>
2020-03-11 02:44:43 +0100

    customizing theme details

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="h-screen flex overflow-hidden bg-red-100">
     <div class="hidden md:flex md:flex-shrink-0">
-          <div class="flex-1 flex flex-col pt-5 pb-4 pr-4 pl-4  bg-red-200">
-            <slot name="sidebarslot"></slot>
-          </div>
+      <div class="flex-1 flex flex-col pt-5 pb-4 pr-4 pl-4  bg-red-200">
+        <div class="items-center flex-shrink-0 px-4">
+          <slot name="sidebarslot"></slot>
+        </div>
+      </div>
     </div>
     <slot name="mainpanelslot"></slot>
   </div>
