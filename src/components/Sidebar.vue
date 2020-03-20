83d35bda7039ac72a32e25c2970e5cd4b7f03d83
Sam <lzanzottera@gmail.com>
2020-03-20 03:16:34 +0100

    improved sidebar on mobile

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -11,9 +11,9 @@
         x-transition:leave-end="opacity-0"
         class="fixed inset-0 z-30 transition-opacity ease-linear duration-300"
       >
-        <div class="absolute inset-0 bg-red-600 opacity-75"></div>
+        <!-- <div class="absolute inset-0 bg-red-600 opacity-75"></div> -->
       </div>
-      <div class="flex fixed inset-0 z-40">
+      <div class="flex fixed inset-0 bg-indigo-600 z-40">
         <div
           v-show="sidebarOpen"
           x-transition:enter-start="-translate-x-full"
@@ -22,90 +22,34 @@
           x-transition:leave-end="-translate-x-full"
           class="flex-1 flex flex-col max-w-xs w-full bg-indigo-400 transform ease-in-out duration-300"
         >
-          <div class="absolute top-0 right-0 -mr-14 p-1">
-            <button
-              v-show="sidebarOpen"
-              @click="sidebarOpen = false"
-              class="flex bg-blue-600 items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
-            >
-              <!-- icona X -->
-              <svg
-                class="h-6 w-6 text-white"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
           <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-            <!-- <div class="flex-shrink-0 flex items-center px-4">
-                <img
-                  class="h-8 w-auto"
-                  src="/img/logos/workflow-logo-on-brand.svg"
-                  alt="Workflow"
-                />
-            </div>-->
             <nav class="mt-5 px-2">
               <img
                 class="inline-block h-32 w-32 rounded-full"
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt
               />
-              <!-- <a
-                  href="#"
-                  class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-indigo-900 focus:outline-none focus:bg-indigo-700 transition ease-in-out duration-150"
-                >
-                  <svg
-                    class="mr-4 h-6 w-6 text-indigo-400 group-hover:text-indigo-300 group-focus:text-indigo-300 transition ease-in-out duration-150"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
-                    />
-                  </svg>
-                  Dashboard
-              </a>-->
             </nav>
           </div>
-          <!-- <div class="flex-shrink-0 flex border-t border-indigo-700 p-4">
-              <a href="#" class="flex-shrink-0 group block focus:outline-none">
-                <div class="flex items-center">
-                  <div>
-                    <img
-                      class="inline-block h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div class="ml-3">
-                    <p class="text-base leading-6 font-medium text-white">
-                      Tom Cook
-                    </p>
-                    <p
-                      class="text-sm leading-5 font-medium text-indigo-300 group-hover:text-indigo-100 group-focus:underline transition ease-in-out duration-150"
-                    >
-                      View profile
-                    </p>
-                  </div>
-                </div>
-              </a>
-          </div>-->
         </div>
-        <!-- <div class="flex-shrink-0 w-14">
-             Force sidebar to shrink to fit close icon 
-        </div>-->
+
+        <div class="absolute top-0 right-0 p-2">
+          <button
+            v-show="sidebarOpen"
+            v-on:click="toggleSidebar"
+            class="flex bg-blue-600 items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+          >
+            <!-- icona X -->
+            <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
 
