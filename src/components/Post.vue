0fc2b4afd9666e16bb33a6ac9173b80f7d715dfa
Sam <lzanzottera@gmail.com>
2020-04-01 02:12:40 +0200

    improving design

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,30 +1,36 @@
 <template>
-  <div class="flex p-6 " :class="{ 'no-border': !border }">
-    <g-link :to="record.path" class="w-5/12  ">
-      <g-image
-        :src="record.image"
-        :alt="record.title"
-        class="rounded-lg object-cover w-full h-full"
-      ></g-image>
-    </g-link>
-    <div class="ml-10 w-7/12  p-8 mr-10">
+  <div class="flex p-6">
+    <div class="w-5/12 shadow-2xl">
       <g-link :to="record.path">
-        <p class="uppercase font-semibold text-blue-800 mt-3  py-2">
+        <g-image
+          :src="record.image"
+          :alt="record.title"
+          class="rounded-lg object-cover w-full h-full "
+        ></g-image>
+      </g-link>
+    </div>
+    <div class="mx-10 w-7/12  p-8">
+      <g-link :to="record.path">
+        <p class="uppercase font-medium text-yellow-700 mt-3  py-2">
           {{ record.category.title }}
         </p>
         <h2 class="text-3xl font-bold py-2 ">
           {{ record.title }}
         </h2>
-        <p class=" pt-3 w-5/6">{{ record.excerpt.substring(0, 699) }}</p>
+        <p class=" pt-3 w-5/6 text-xl">
+          {{ record.excerpt.substring(0, 699) }}
+        </p>
       </g-link>
-      <p class=" py-2 text-l font-bold">
-        <g-link :to="record.path" class="w-5/12">
-          <a href="https://css-tricks.com" class="text-blue-800"
+      <p class=" py-2 ">
+        <g-link :to="record.path" class="w-5/12 ">
+          <a
+            href="https://css-tricks.com"
+            class="text-blue-600 hover:text-indigo-700 text-lg font-bold"
             >Continua a leggere...</a
           >
         </g-link>
       </p>
-      <div class="text-xs leading-none uppercase py-3 ">
+      <div class="text-base font-light italic leading-none uppercase py-3 ">
         <time :datetime="record.datetime">{{ record.humanTime }}</time>
       </div>
     </div>
