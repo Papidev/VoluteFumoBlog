aba6b06ea0da7c8b8132af85e8ef559f0acba540
Sam <lzanzottera@gmail.com>
2020-04-11 01:55:34 +0200

    new social component

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -8,25 +8,39 @@
       class="h-40 w-40 rounded-full m-3"
     />
 
-    <div class="block p-4 w-full  h-72">
+    <div class="block p-4 w-full h-72">
       contentino
-      <div>
-        <a
-          target="_blank"
-          href="https://it-it.facebook.com/fgiustiziero"
-        ><svg
-        
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 408.79 408.79"
-          class="m-2 h-8 w-8"
-        >
-          <title>facebook icon link</title>
-          <path
-            d="M353.7 0H55.1A55.09 55.09 0 000 55.09V353.7A55.08 55.08 0 0055.1 408.8h147.27l.25-146.08h-37.95a8.95 8.95 0 01-8.95-8.92l-.18-47.09a8.95 8.95 0 018.95-8.99h37.88v-45.5c0-52.8 32.25-81.55 79.35-81.55h38.65c4.95 0 8.96 4.01 8.96 8.96v39.7c0 4.95-4.01 8.95-8.95 8.96h-23.72c-25.62 0-30.58 12.18-30.58 30.04v39.4h56.29a8.95 8.95 0 018.89 10l-5.58 47.09a8.96 8.96 0 01-8.9 7.9h-50.45l-.25 146.08h87.63a55.08 55.08 0 0055.09-55.09V55.08A55.09 55.09 0 00353.7 0z"
-            fill="#475993"
-          /></svg>
-        </a>
-      </div>
+      <social-link
+        v-for="social in socials"
+        :key="social.id"
+        :svg="social.svg"
+        :link="social.link"
+      />
     </div>
   </div>
 </template>
+<script>
+import Social from "./Social";
+export default {
+  components: {
+    "social-link": Social,
+  },
+  data() {
+    return {
+      socials: [
+        {
+          id: "1",
+          svg: {
+            title: "facebook icon link",
+            viewbox: "0 0 408.79 408.79",
+            fillColor: "#475993",
+            d:
+              "M353.7 0H55.1A55.09 55.09 0 000 55.09V353.7A55.08 55.08 0 0055.1 408.8h147.27l.25-146.08h-37.95a8.95 8.95 0 01-8.95-8.92l-.18-47.09a8.95 8.95 0 018.95-8.99h37.88v-45.5c0-52.8 32.25-81.55 79.35-81.55h38.65c4.95 0 8.96 4.01 8.96 8.96v39.7c0 4.95-4.01 8.95-8.95 8.96h-23.72c-25.62 0-30.58 12.18-30.58 30.04v39.4h56.29a8.95 8.95 0 018.89 10l-5.58 47.09a8.96 8.96 0 01-8.9 7.9h-50.45l-.25 146.08h87.63a55.08 55.08 0 0055.09-55.09V55.08A55.09 55.09 0 00353.7 0z",
+          },
+          link: "https://it-it.facebook.com/fgiustiziero",
+        },
+      ],
+    };
+  },
+};
+</script>
