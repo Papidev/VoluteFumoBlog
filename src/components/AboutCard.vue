f31c001af4f533efeaa61d8680b510c52a5a3be4
Sam <lzanzottera@gmail.com>
2020-04-13 01:34:05 +0200

    contact.vue

--- src/components/AboutCard.vue
+++ src/components/AboutCard.vue
@@ -8,36 +8,60 @@
       class="h-40 w-40 rounded-full m-3"
     />
 
-    <div class="block p-4 w-full h-72">
-      contentino
-      <social-link
-        v-for="social in socials"
-        :key="social.id"
-        :svg="social.svg"
-        :link="social.link"
-      />
+    <div class="p-4 w-full h-72">
+      I miei social
+      <div class="flex">
+        <contact
+          v-for="social in socials"
+          :key="social.id"
+          :name="social.name"
+          :link="social.link"
+          class="m-1 rounded-lg"
+        />
+      </div>
+      <div class="flex flex-col mt-4">
+        Puoi contattarmi anche via mail
+        <contact
+          v-for="contact in contacts"
+          :key="contact.id"
+          :name="contact.name"
+          :link="contact.link"
+          class="m-1"
+        />
+      </div>
     </div>
   </div>
 </template>
 <script>
-import Social from "./Social";
+import Contact from "./Contact";
 export default {
   components: {
-    "social-link": Social,
+    Contact,
   },
   data() {
     return {
       socials: [
         {
-          id: "1",
-          svg: {
-            title: "facebook icon link",
-            viewbox: "0 0 408.79 408.79",
-            fillColor: "#475993",
-            d:
-              "M353.7 0H55.1A55.09 55.09 0 000 55.09V353.7A55.08 55.08 0 0055.1 408.8h147.27l.25-146.08h-37.95a8.95 8.95 0 01-8.95-8.92l-.18-47.09a8.95 8.95 0 018.95-8.99h37.88v-45.5c0-52.8 32.25-81.55 79.35-81.55h38.65c4.95 0 8.96 4.01 8.96 8.96v39.7c0 4.95-4.01 8.95-8.95 8.96h-23.72c-25.62 0-30.58 12.18-30.58 30.04v39.4h56.29a8.95 8.95 0 018.89 10l-5.58 47.09a8.96 8.96 0 01-8.9 7.9h-50.45l-.25 146.08h87.63a55.08 55.08 0 0055.09-55.09V55.08A55.09 55.09 0 00353.7 0z",
-          },
+          id: "s1",
+          name: "facebook",
           link: "https://it-it.facebook.com/fgiustiziero",
+        },
+        {
+          id: "s2",
+          name: "instagram",
+          link: "https://it-it.facebook.com/fgiustiziero",
+        },
+        {
+          id: "s3",
+          name: "pinterest",
+          link: "https://www.pinterest.it/striapiratesa/",
+        },
+      ],
+      contacts: [
+        {
+          id: "c1",
+          name: "email",
+          link: "mailto:federica.giustiziero@gmail.com",
         },
       ],
     };
---
+++ src/components/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <a target="_blank" :href="link"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        :viewBox="svg.viewbox"
+        class="m-2 h-8 w-8"
+      >
+        <title>${{ svg.title }}</title>
+        <path :d="svg.d" :fill="svg.fillColor" />
+      </svg>
+    </a> -->
+  <!-- <instagram class="w-20 h-20"></instagram>
+    <facebook class="bg-gray-800 w-20 h-20"></facebook> -->
+  <component :is="dynamicComponent" class="w-10 h-10"></component>
+</template>
+<script>
+// import Instagram from "../assets/svgs/Instagram.svg";
+// import Facebook from "../assets/svgs/Facebook.svg";
+export default {
+  components: {
+    // instagram: Instagram,
+    // facebook: Facebook,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    dynamicComponent() {
+      return () => import(`../assets/svgs/${this.name}.svg`);
+    },
+  },
+};
+</script>
