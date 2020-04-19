f0b58f76b5516427a83ff1ac844390b3768ad74e
Sam <lzanzottera@gmail.com>
2020-04-19 03:00:07 +0200

    improving design

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: "volute di fumo",
+  siteName: "Volute di Fumo",
   siteDescription: "blogging around",
   siteUrl: "http://localhost:8080",
   plugins: [
--- src/layouts/partials/HeaderWithNavbar.vue
+++ src/layouts/partials/HeaderWithNavbar.vue
@@ -2,7 +2,7 @@
   <header id="header">
     <nav class="z-50 h-auto">
       <div class="flex items-center">
-        <div class="flex-grow text-sm uppercase bg-blue-700 p-3">
+        <div class="flex-grow text-xs uppercase px-3">
           <ul class="flex list-none justify-left text-gray-400">
             <li
               v-for="(element, index) in $static.metadata.navigation"
@@ -27,7 +27,7 @@
           </ul>
         </div>
 
-        <div class="text-white p-2">
+        <div class="text-white px-2 p-1">
           <ul class="flex list-none justify-center">
             <li class="mr-3">
               <ThemeSwitcher />
@@ -56,20 +56,25 @@
       </div>
     </nav>
 
-    <div
-      class="bgimage w-screen pt-8 md:pt-16 text-center items-center text-chica-violet h-48 lg:h-72"
-    >
-      <h2 class="text-indigo-700">
-        <span class="text-4xl">
-          <font-awesome
-            :icon="['fas', 'ghost']"
-            class="mb-1 mr-4"
-          ></font-awesome>
-        </span>
-        <span class="text-5xl">{{ $static.metadata.siteName }}</span>
-      </h2>
-      <div class="text-indigo-600 text-xl">
-        {{ $static.metadata.siteDescription }}
+    <div class="bgimage w-screen pt-12 text-purple-900 h-48 lg:h-64">
+      <div
+        class="pl-24 w-auto inline-block px-2 rounded-lg items-center headertitle italic border-1 border-orange-800 border-1 border-chica-violet"
+      >
+        <h2 class="p-1">
+          <span class="text-3xl md:text-4xl">
+            <font-awesome
+              :icon="['fas', 'ghost']"
+              class="mb-1 mr-4"
+            ></font-awesome>
+          </span>
+          <span class="text-3xl md:text-5xl">{{
+            $static.metadata.siteName
+          }}</span>
+        </h2>
+
+        <!-- <div class="text-white-600 text-xl">
+          {{ $static.metadata.siteDescription }}
+        </div> -->
       </div>
     </div>
   </header>
@@ -104,10 +109,16 @@ export default {
 </static-query>
 <style lang="scss" scoped>
 .bgimage {
-  background: url("../../assets/fuji_sunset.jpg") no-repeat center top;
+  //background: url("../../assets/fuji_sunset.jpg") no-repeat center top;
+  background: url("../../assets/people-walking-on-the-street-2506923.jpg")
+    no-repeat left center;
   // -webkit-background-size: cover;
   // -moz-background-size: cover;
   // -o-background-size: cover;
   // background-size: auto;
+}
+
+.headertitle {
+  background: rgba(248, 237, 217, 0.9); /* Green background with 30% opacity */
 }
 </style>
