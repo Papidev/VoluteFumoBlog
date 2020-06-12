3bc807d5253dceed2cbd7a5b8b9542a2cd3c1654
Sam <lzanzottera@gmail.com>
2020-06-12 18:18:45 +0200

    back link in Post page

--- src/layouts/PostLayout.vue
+++ src/layouts/PostLayout.vue
@@ -1,130 +1,143 @@
 <template>
   <div>
     <header-bar />
+
     <div class="bg-chica-greenish">
-      <div class="flex flex-col items-center mx-48">
-        <!-- image -->
-        <div
-          class="flex flex-col items-center border-chica-green border-l-2 border-r-2 border-b-2 rounded-b-lg"
-        >
-          <g-image
-            :src="$page.blog.image"
-            class="h-104 object-cover w-full"
-          ></g-image>
-
-          <!-- logo -->
-          <div class="-my-16 z-10">
+      <div class="flex">
+        <div class="text-chica-dark">
+          <a @click="$router.go(-1)"
+            ><backArrow class="h-10 w-10 ml-3 mt-14 fill-current"></backArrow>
+          </a>
+          <h1 class="p-2">
+            Previous
+          </h1>
+        </div>
+        <div class="flex flex-col items-center mx-48">
+          <!-- image -->
+          <div
+            class="flex flex-col items-center border-chica-green border-l-2 border-r-2 border-b-2 rounded-b-lg"
+          >
             <g-image
-              src="@/assets/img/Volute_logo.png"
-              alt="Federica Giustiziero photo"
-              class="h-24 w-24 logo-round-orange"
-            />
-          </div>
+              :src="$page.blog.image"
+              class="h-104 object-cover w-full"
+            ></g-image>
 
-          <!-- header -->
-          <div class="bg-chica-dark p-6 mt-6">
-            <span
-              class="uppercase text-sm font-semibold text-chica-pinkish py-2 tracking-wide"
-            >
-              <g-link :to="$page.blog.category.path" class="hover:underline">{{
-                $page.blog.category.title
-              }}</g-link>
-            </span>
-            <h1 class="text-5xl font-bold py-2 text-chica-light-yellowish">
-              {{ $page.blog.title }}
-            </h1>
+            <!-- logo -->
+            <div class="-my-16 z-10">
+              <g-image
+                src="@/assets/img/Volute_logo.png"
+                alt="Federica Giustiziero photo"
+                class="h-24 w-24 logo-round-orange"
+              />
+            </div>
+
+            <!-- header -->
+            <div class="bg-chica-dark p-6 mt-6">
+              <span
+                class="uppercase text-sm font-semibold text-chica-pinkish py-2 tracking-wide"
+              >
+                <g-link
+                  :to="$page.blog.category.path"
+                  class="hover:underline"
+                  >{{ $page.blog.category.title }}</g-link
+                >
+              </span>
+              <h1 class="text-5xl font-bold py-2 text-chica-light-yellowish">
+                {{ $page.blog.title }}
+              </h1>
+              <div
+                class="h-12 overflow-hidden text-chica-light-yellowish"
+                v-html="$page.blog.excerpt"
+              ></div>
+            </div>
+
+            <!-- body -->
             <div
-              class="h-12 overflow-hidden text-chica-light-yellowish"
-              v-html="$page.blog.excerpt"
+              class="text-chica-dark bg-chica-light-yellowish p-10 text-xl"
+              v-html="$page.blog.content"
             ></div>
           </div>
+          <div class="bg-chica-greenish p-3 w-full div mt-2">
+            <div class="flex items-center">
+              <div class="flex justify-between items-center">
+                <ul class="list-none flex author-list">
+                  <li
+                    v-for="author in $page.blog.author"
+                    :key="author.id"
+                    class="author-list-item"
+                  >
+                    <g-link :to="author.path">
+                      <g-image
+                        :src="author.image"
+                        :alt="author.name"
+                        class="h-10 w-10 sm:h-10 sm:w-10 rounded-full border-2 border-chica-orange"
+                      />
+                    </g-link>
+                  </li>
+                </ul>
+              </div>
+              <div class="pl-3 flex flex-col text-xs leading-none uppercase">
+                <p>
+                  <span
+                    v-for="(author, index) in $page.blog.author"
+                    :key="author.id"
+                  >
+                    <g-link :to="author.path" class="hover:underline p-1">{{
+                      author.name
+                    }}</g-link>
+                    <span v-if="index < $page.blog.author.length - 1">,</span>
+                  </span>
+                </p>
+                <p class="text-gray-700 p-1">
+                  <time :datetime="$page.blog.datetime">{{
+                    $page.blog.humanTime
+                  }}</time>
+                </p>
+              </div>
+            </div>
+          </div>
 
-          <!-- body -->
+          <!-- tags -->
+          <div class="mt-5">
+            <g-link
+              v-for="tag in $page.blog.tags"
+              :key="tag.id"
+              :to="tag.path"
+              class="py-3 px-6 mr-2 text-sm font-semibold tracking-widest bg-chica-orange bg-opacity-85 hover:text-chica-green hover:bg-opacity-50 border-chica-dark text-white rounded-full"
+              >{{ tag.title }}</g-link
+            >
+          </div>
+
+          <!-- Post Thumb list -->
           <div
-            class="text-chica-dark bg-chica-light-yellowish p-10 text-xl"
-            v-html="$page.blog.content"
-          ></div>
+            class="w-full overflow-hidden bg-chica-light-pinkish mt-12 border-3 border-chica-dark rounded-lg"
+          >
+            <div
+              class="w-64 bg-chica-dark rounded-br-lg border-chica-dark border-2"
+            >
+              <h1 class="text-2xl text-chica-pinkish font-semibold p-2">
+                Potrebbe interessarti
+              </h1>
+            </div>
+
+            <div class="flex flex-wrap p-3">
+              <post-thumb
+                v-if="$page.previous"
+                :record="$page.previous"
+                :border="false"
+                class="p-3 mt-2"
+              ></post-thumb>
+              <post-thumb
+                v-if="$page.next"
+                :record="$page.next"
+                :border="false"
+                class="p-3 mt-2"
+              ></post-thumb>
+            </div>
+          </div>
         </div>
 
         <!-- autore -->
-        <div class="bg-chica-greenish p-3 w-full div mt-2">
-          <div class="flex items-center">
-            <div class="flex justify-between items-center">
-              <ul class="list-none flex author-list">
-                <li
-                  v-for="author in $page.blog.author"
-                  :key="author.id"
-                  class="author-list-item"
-                >
-                  <g-link :to="author.path">
-                    <g-image
-                      :src="author.image"
-                      :alt="author.name"
-                      class="h-10 w-10 sm:h-10 sm:w-10 rounded-full border-2 border-chica-orange"
-                    />
-                  </g-link>
-                </li>
-              </ul>
-            </div>
-            <div class="pl-3 flex flex-col text-xs leading-none uppercase">
-              <p>
-                <span
-                  v-for="(author, index) in $page.blog.author"
-                  :key="author.id"
-                >
-                  <g-link :to="author.path" class="hover:underline p-1">{{
-                    author.name
-                  }}</g-link>
-                  <span v-if="index < $page.blog.author.length - 1">,</span>
-                </span>
-              </p>
-              <p class="text-gray-700 p-1">
-                <time :datetime="$page.blog.datetime">{{
-                  $page.blog.humanTime
-                }}</time>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <!-- tags -->
-        <div class="mt-5">
-          <g-link
-            v-for="tag in $page.blog.tags"
-            :key="tag.id"
-            :to="tag.path"
-            class="py-3 px-6 mr-2 text-sm font-semibold tracking-widest bg-chica-orange bg-opacity-85 hover:text-chica-green hover:bg-opacity-50 border-chica-dark text-white rounded-full"
-            >{{ tag.title }}</g-link
-          >
-        </div>
-
-        <!-- Post Thumb list -->
-        <div
-          class="w-full overflow-hidden bg-chica-light-pinkish mt-12 border-3 border-chica-dark rounded-lg"
-        >
-          <div
-            class="w-64 bg-chica-dark rounded-br-lg border-chica-dark border-2"
-          >
-            <h1 class="text-2xl text-chica-pinkish font-semibold p-2">
-              Potrebbe interessarti
-            </h1>
-          </div>
-
-          <div class="flex flex-wrap p-3">
-            <post-thumb
-              v-if="$page.previous"
-              :record="$page.previous"
-              :border="false"
-              class="p-3 mt-2"
-            ></post-thumb>
-            <post-thumb
-              v-if="$page.next"
-              :record="$page.next"
-              :border="false"
-              class="p-3 mt-2"
-            ></post-thumb>
-          </div>
-        </div>
       </div>
       <footer-bar class="mt-8" />
     </div>
@@ -134,11 +147,14 @@
 import PostThumb from "~/components/PostThumb";
 import Header from "@/layouts/Partials/Header";
 import Footer from "@/layouts/Partials/Footer";
+import backArrow from "../assets/svgs/backArrow.svg";
+
 export default {
   components: {
     PostThumb,
     "header-bar": Header,
     "footer-bar": Footer,
+    backArrow,
   },
 };
 </script>
