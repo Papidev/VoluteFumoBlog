1cb64a9a7fa2150fc1cd50fc2c015a99d6ca68c2
Sam <lzanzottera@gmail.com>
2020-06-24 17:58:35 +0200

    won fight against image positiong in CardArgument component

--- src/components/CardArgument.vue
+++ src/components/CardArgument.vue
@@ -1,7 +1,7 @@
 <template>
-  <g-link :to="`/${this.title}/`">
-    <div class="flex items-start" :class="reverse">
-      <div class="flex flex-col p-3">
+  <div class="flex items-start" :class="reverse">
+    <div class="flex flex-col p-3 w-1/2">
+      <g-link :to="`/${this.title}/`">
         <h1
           class="text-2xl font-bold tracking-wide text-chica-orange text-opacity-85"
         >
@@ -10,15 +10,15 @@
         <div class="mt-1 overflow-hidden">
           <slot name="intro"></slot>
         </div>
-      </div>
-      <div class="ml-6 py-3 pr-3 w-256">
-        <g-image
-          :src="`../assets/img/${this.title}.jpg`"
-          class="object-cover rounded-lg"
-        />
-      </div>
+      </g-link>
     </div>
-  </g-link>
+    <div class="self-center ml-6 py-2 pr-3 w-1/2">
+      <g-image
+        :src="`../assets/img/${this.title}.jpg`"
+        class="object-cover h-56 rounded-lg"
+      />
+    </div>
+  </div>
 </template>
 <script>
 export default {
